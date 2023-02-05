9f843fc3f2198e6ffe2ec7675aaf42e6f31af5c1
54513e <tsukasa1130@outlook.jp>
2023-02-06 02:32:22 +0900

    memo

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
   channel_name = models.CharField("チャンネル名", max_length=255)
   caption = models.TextField("説明")
-  video_url = models.URLField("動画URL", max_length=255) # TODO: クラウドに保存
+  video_url = models.URLField("動画URL", max_length=255) 
   music_name = models.CharField("音楽名", max_length=255)
   created_at = models.DateTimeField("作成日", auto_now_add=True)
 
